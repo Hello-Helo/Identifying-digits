7a496e925f907661b33845ad574f75f4288bbf1a
Helo Lazari <heloisalbento@gmail.com>
2020-10-23 18:29:51 -0300

    Testing git-vim integration

--- EP-T1.py
+++ EP-T1.py
@@ -1,1 +1,11 @@
 # Correspondente a parimeira tarefa do EP de Computação III
+
+import NumPy as np
+import Matplotlib as mp
+
+def rot(W, n, m, i, j, c, s)
+
+# Resolver Wx = b usando uma transformação tal que W_{nxm} vira R, uma
+# matriz triangular superior
+
+W = np.matrix()
