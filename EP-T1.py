c4e19e3f0eccf2c5d4286a5f4a8ff2df7f21b7e7
Helo Lazari <heloisalbento@gmail.com>
2020-10-24 10:28:20 -0300

    Adicionar o método de rotação de Givens

--- EP-T1.py
+++ EP-T1.py
@@ -3,10 +3,32 @@
 import NumPy as np
 import Matplotlib as mp
 
-def rot(W, n, m, i, j, c, s)
-
 # Resolver Wx = b usando uma transformação tal que W_{nxm} vira R, uma
 # matriz triangular superior
 
-W = np.matrix()
-b = np.matrix()
+#########################################################################
+
+# Método de rotação de Givens disponibilizado no E-Disciplinas no arquivo
+# vector_oper.py
+def Rotgivens(W,vecaux,n,m,i,j,c,s):
+  vecaux[0:m] = c * W[i,0:m] - s * W[j,0:m]
+  W[j,0:m] = s * W[i,0:m] + c * W[j,0:m]
+  W[i,0:m] = vecaux[0:m]
+  return W
+
+#########################################################################
+
+#Tamanho da matriz W
+n = 784
+m = 100000
+
+# Preenche com qualquer coisa
+W = np.random.rand(n, m)
+vecaux = np.random.rand(m)
+
+#Fazemos uma copia para guardar a original
+A = W.copy()
+
+#Vamos aplicar Givens nessas linhas
+i = 4
+j = 8
