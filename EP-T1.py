c5d01c29686204838eb3c3e2db85c6c7029270da
Helo Lazari <heloisalbento@gmail.com>
2020-10-25 20:01:32 -0300

    Método de triangulação superior funcionando para matrizes determinadas

--- EP-T1.py
+++ EP-T1.py
@@ -1,20 +1,18 @@
 # Correspondente a parimeira tarefa do EP de Computação III
 
-import numpy as np
-import math
-
 # Resolver Wx = b usando uma transformação tal que W_{nxm} vira R, uma
 # matriz triangular superior
+
+import numpy as np
+import math
 
 #########################################################################
 
 # Método de rotação de Givens disponibilizado no E-Disciplinas no arquivo
 # vector_oper.py
-def Rotgivens(W,vecaux,n,m,i,j,c,s):
-    vecaux[0:m] = c * W[i,0:m] - s * W[j,0:m]
-    W[j,0:m] = s * W[i,0:m] + c * W[j,0:m]
-    W[i,0:m] = vecaux[0:m]
-    return W
+def Rotgivens(W,n,m,i,j,c,s):
+  W[i,0:m] , W[j,0:m] = c * W[i,0:m] - s * W[j,0:m] , s * W[i,0:m] + c * W[j,0:m]
+  return W
 
 #########################################################################
 
@@ -34,15 +32,17 @@ def Is_tsup(W):
 n = 3
 m = 3
 
-# Preenche com qualquer coisa
-W = [[6,5,0],[5,1,4],[0,4,3]]
+# Cria a matriz, aleatoriamente ou não
+# W = np.random.rand(n, m)
+W = np.array([[6,5,0],[5,1,4],[0,4,3]])
 vecaux = np.random.rand(m)
 
 #Fazemos uma copia para guardar a original
 A = W.copy()
 
-#Para simplificar, usamos valores da matriz de rotacao quaisquer para teste
-#  mas garantimos que matriz seja ortogonal (para os testes de performace isso nao importa)
+# Para simplificar, usamos valores da matriz de rotacao quaisquer para
+# teste mas garantimos que matriz seja ortogonal (para os testes de
+# performace isso nao importa)
 c = 0.5
 s = math.sqrt(3.0)/2.0
 
@@ -57,7 +57,7 @@ while tsup != True:
     for j in range(0, m):
         for i in range(j + 1, n):
             if W[i][j] != 0:
-                W = Rotgivens(W,vecaux,n,m,i,j,c,s)
+                W = Rotgivens(W,n,m,i,j,c,s)
     tsup = Is_tsup(W)
     #print(tsup)
 
