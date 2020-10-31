dc734b90bdae5598db7c45929e2ea24d9b6204d8
Helo Lazari <heloisalbento@gmail.com>
2020-10-31 14:36:29 -0300

    Arrumando a formatação

--- EP-T1.py
+++ EP-T1.py
@@ -3,21 +3,25 @@
 # Resolver Wx = b usando uma transformação tal que W_{nxm} vira R, uma
 # matriz triangular superior
 
-import numpy as np
 import math
+
+import numpy as np
 
 #########################################################################
 
 # Método de rotação de Givens disponibilizado no E-Disciplinas no arquivo
 # vector_oper.py
 
-def Rotgivens(W,n,m,i,j,c,s):
-  W[i,0:m] , W[j,0:m] = c * W[i,0:m] - s * W[j,0:m] , s * W[i,0:m] + c * W[j,0:m]
-  return W
+
+def Rotgivens(W, n, m, i, j, c, s):
+    W[i, 0:m], W[j, 0:m] = c * W[i, 0:m] - s * W[j, 0:m], s * W[i, 0:m] + c * W[j, 0:m]
+    return W
+
 
 #########################################################################
 
 # Função para verifiar se uma matriz é triangular superior
+
 
 def Is_tsup(W, Wn, Wm):
     is_valid = True
@@ -28,21 +32,19 @@ def Is_tsup(W, Wn, Wm):
                 return is_valid
     return is_valid
 
+
 #########################################################################
-
-# public static void main(String args[])
-
 
 # Cria a matriz, aleatoriamente ou não
 # W = np.random.rand(n, m)
-W = np.array([[6,5,0],[5,1,4],[0,4,1]], dtype = float)
-b = np.array([[16],[19],[11]], dtype = float)
+W = np.array([[6, 5, 0], [5, 1, 4], [0, 4, 1]], dtype=float)
+b = np.array([[16], [19], [11]], dtype=float)
 
-#Tamanho da matriz W
+# Tamanho da matriz W
 Wn = np.atleast_2d(W).shape[0]
 Wm = np.atleast_2d(W).shape[1]
 
-#Tamanho da matriz b
+# Tamanho da matriz b
 bn = np.atleast_2d(b).shape[0]
 bm = np.atleast_2d(b).shape[1]
 
@@ -50,50 +52,51 @@ bm = np.atleast_2d(b).shape[1]
 # teste mas garantimos que matriz seja ortogonal (para os testes de
 # performace isso nao importa)
 c = 0.5
-s = math.sqrt(3.0)/2.0
-t=0
+s = math.sqrt(3.0) / 2.0
+t = 0
 
 # Printa a matriz inicial
-print('A matriz W original:')
-print(W, end = "\n")
-print('A matriz b original:')
-print(b, end = "\n")
+print("A matriz W original:")
+print(W, end="\n")
+print("A matriz b original:")
+print(b, end="\n")
 
 # Boolean que verifica se a matriz é triangular superior
 tsup = False
 
 # Loop que aplica RotGivens para todo elemento inferior a diagonal enquanto
 # ela não é triangular superior
-while tsup != True:
+while tsup == False:
     for k in range(0, Wm):
-        for j in range(Wn-1,k,-1):
-            i = j-1
-            if W[j,k] != 0:
-            	if abs(W[i,k]) > abs(W[j,k]):
-            		t = -W[j,k]/W[i,k]
-            		c = 1/math.sqrt(1 + t**2)
-            		s = c*t
-            	else:
-            		t = -W[i,k]/W[j,k]
-            		s = 1/math.sqrt(1 + t**2)
-            		c = s*t
-            	W = Rotgivens(W,Wn,Wm,i,j,c,s)
-            	if k < bm:
-                    b = Rotgivens(b,Wn,Wm,i,j,c,s)
+        for j in range(Wn - 1, k, -1):
+            i = j - 1
+            if W[j, k] != 0:
+                if abs(W[i, k]) > abs(W[j, k]):
+                    t = -W[j, k] / W[i, k]
+                    c = 1 / math.sqrt(1 + t ** 2)
+                    s = c * t
+                else:
+                    t = -W[i, k] / W[j, k]
+                    s = 1 / math.sqrt(1 + t ** 2)
+                    c = s * t
+                W = Rotgivens(W, Wn, Wm, i, j, c, s)
+                if k < bm:
+                    b = Rotgivens(b, Wn, Wm, i, j, c, s)
     tsup = Is_tsup(W, Wn, Wm)
 
 
-print('A matriz após a rotação:')
-print(W, end = "\n")
-print('O vetor b após a rotação:')
-print(b, end = "\n")
+print("A matriz após a rotação:")
+print(W, end="\n")
+print("O vetor b após a rotação:")
+print(b, end="\n")
+
 
 # Soluciona a equação Rx = b'e printa a matrix x resultante
-x = np.array([[0],[0],[0]], dtype = float)
+x = np.array([[0], [0], [0]], dtype=float)
 som = 0
-for k in range (Wm-1, -1, -1):
-	for j in range (k+1, Wm):
-		som = W[k, j]*x[j, 0]
-	x[k, 0] = (b[k, 0] - som)/W[k, k]
-print('A matriz após a rotação:')
+for k in range(Wm - 1, -1, -1):
+    for j in range(k + 1, Wm):
+        som = W[k, j] * x[j, 0]
+    x[k, 0] = (b[k, 0] - som) / W[k, k]
+print("A matriz após a rotação:")
 print(x)
