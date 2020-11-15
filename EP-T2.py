820fe6cda3c51bb988dec3f8fb205d6c79ccef62
Helo Lazari <heloisalbento@gmail.com>
2020-11-15 14:53:29 -0300

    Promeblas na função de normalização arrumado - Divisão por 0 ainda ocorre

--- EP-T2.py
+++ EP-T2.py
@@ -19,15 +19,62 @@ import numpy as np
 ###############################################################################
 
 
-def Normalize(W):
+def Rotgivens(W, n, m, i, j, c, s):
+    W[i, 0:m], W[j, 0:m] = c * W[i, 0:m] - s * W[j, 0:m], s * W[i, 0:m] + c * W[j, 0:m]
+    return W
+
+
+###############################################################################
+
+
+def Is_tsup(W, Wn, Wm):
+    is_valid = True
+    for collum in range(0, Wm):
+        for line in range(collum + 1, Wn):
+            if W[line][collum] != 0:
+                is_valid = False
+                return is_valid
+    return is_valid
+
+
+###############################################################################
+
+
+def Constants(W, j, k, i):
+    if abs(W[i, k]) > abs(W[j, k]):
+        t = -W[j, k] / W[i, k]
+        c = 1 / math.sqrt(1 + t ** 2)
+        s = c * t
+    else:
+        t = -W[i, k] / W[j, k]
+        s = 1 / math.sqrt(1 + t ** 2)
+        c = s * t
+    return s, c
+
+
+###############################################################################
+
+
+def Transformation(W, b):
+    tsup = False
     Wn = np.atleast_2d(W).shape[0]
     Wm = np.atleast_2d(W).shape[1]
-    for j in range(0, Wn):
-        s = math.sqrt((np.sum(W[0:Wn, j]**2))
-        for n in range(0, An):
-            W[n, m] = W[n, m] / Sj
-        W[0: Wn, j] = W[0: Wn, j]/s
-    return W
+    for k in range(0, Wm):
+        for j in range(Wn - 1, k, -1):
+            i = j - 1
+            if W[j, k] != 0:
+                const = Constants(W, j, k, i)
+                s = const[0]
+                c = const[1]
+                W = Rotgivens(W, Wn, Wm, i, j, c, s)
+                b = Rotgivens(b, Wn, Wm, i, j, c, s)
+    tsup = Is_tsup(W, Wn, Wm)
+    if tsup is False:
+        mat = Transformation(W, b)
+        W = mat[0]
+        b = mat[1]
+    return W, b
+
 
 ###############################################################################
 
@@ -50,19 +97,33 @@ def Solution(W, b):
 ###############################################################################
 
 
-def Make_positive(W):
+def Normalize(W):
     Wn = np.atleast_2d(W).shape[0]
-    Wm = np.atleast_2d(b).shape[1]
-        for i in range(0, Wn):
-            for j in range(0, Wm):
-                W[i, j] = max(0, W[1, j])
+    Wm = np.atleast_2d(W).shape[1]
+    for j in range(0, Wm):
+        s = math.sqrt((np.sum(W[0:Wn, j] ** 2)))
+        # for i in range(0, An):
+        #     W[i, j] = W[i, j] / s
+        W[0:Wn, j] = W[0:Wn, j] / s
     return W
 
 
 ###############################################################################
 
 
-A = [[3 / 10, 3 / 5, 0], [1 / 2, 0, 1], [4 / 10, 4 / 5, 0]]
+def Make_positive(W):
+    Wn = np.atleast_2d(W).shape[0]
+    Wm = np.atleast_2d(W).shape[1]
+    for i in range(0, Wn):
+        for j in range(0, Wm):
+            W[i, j] = max(0, W[1, j])
+    return W
+
+
+###############################################################################
+
+
+A = np.array([[3 / 10, 3 / 5, 0], [1 / 2, 0, 1], [4 / 10, 4 / 5, 0]])
 An = np.atleast_2d(A).shape[0]
 Am = np.atleast_2d(A).shape[1]
 
@@ -75,6 +136,11 @@ Arb = 2
 W = np.random.random(size=(An, Arb))
 H = np.empty((Arb, Am), dtype=float)
 
+print("A matriz A original:")
+print(A, end="\n")
+print("A matriz W original:")
+print(W, end="\n")
+
 Aprime = np.copy(A)
 
 E = 10
@@ -83,7 +149,7 @@ iterations = 0
 while E > 0.000001 and iterations < 100:
     W = Normalize(W)
 
-    Transf = EP-T1.Transformation(W, A)
+    Transf = Transformation(W, A)
     W = Transf[0]
     A = Transf[1]
     H = Solution(W, A)
@@ -95,7 +161,7 @@ while E > 0.000001 and iterations < 100:
     At = np.transpose(A)
     Ht = np.transpose(H)
 
-    Transf = EP-T1.Transformation(Ht, At)
+    Transf = Transformation(Ht, At)
     Ht = Transf[0]
     At = Transf[1]
     Wt = Solution(Ht, At)
@@ -105,5 +171,5 @@ while E > 0.000001 and iterations < 100:
     W = np.transpose(Wt)
     W = Make_positive(W)
 
-    E = (A - W*H)**2
+    E = (A - W * H) ** 2
     iterations += 1
