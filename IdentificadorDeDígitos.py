dc92be0003621df771efa90328a13018039cf597
Helo Lazari <heloisalbento@gmail.com>
2020-11-20 18:15:39 -0300

    Add IdentificadorDeDígitos.py e os arquivos .txt para teste

---
+++ IdentificadorDeDígitos.py
@@ -0,0 +1,38 @@
+#      _   _ _
+#     | | | | |        Heloisa Lazari & Lucca Alipio
+#     | |_| | |        Ciências Moleculares - USP
+#     |  _  | |___     heloisalbento@usp.br | luccapagnan@usp.br
+#     |_| |_|_____|
+#
+#     Terceira tarefa do EP de Computação III
+#
+#
+#
+#
+###############################################################################
+
+import math
+
+import numpy as np
+
+###############################################################################
+
+
+def Train():
+    return
+
+
+###############################################################################
+
+W0 = np.loadtxt("train_dig0.txt")
+W1 = np.loadtxt("train_dig1.txt")
+W2 = np.loadtxt("train_dig2.txt")
+W3 = np.loadtxt("train_dig3.txt")
+W4 = np.loadtxt("train_dig4.txt")
+W5 = np.loadtxt("train_dig5.txt")
+W6 = np.loadtxt("train_dig6.txt")
+W7 = np.loadtxt("train_dig7.txt")
+W8 = np.loadtxt("train_dig8.txt")
+W9 = np.loadtxt("train_dig9.txt")
+
+p = 10
